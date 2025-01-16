133139d79ddc9383feec30269d45141ff184a2d3
Omkar Sawant Bhosle <omkarsawantbhosle@Omkars-MacBook-Pro.local>
2025-01-15 17:55:32 -0800

    Initial commit for Recruiter Omegle MVP

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_socketio import SocketIO
+# from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-socketio = SocketIO(app)
+# socketio = SocketIO(app)
 
 from app import routes, video, dashboard, swiping
