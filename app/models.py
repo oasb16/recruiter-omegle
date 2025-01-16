acf8c0cf62066a8f53d29df57fcd9cd2029d5a77
Omkar Sawant Bhosle <omkarsawantbhosle@Omkars-MacBook-Pro.local>
2025-01-15 17:38:38 -0800

    Initial commit for Recruiter Omegle MVP

--- app/models.py
+++ app/models.py
@@ -3,6 +3,21 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    resume = db.Column(db.Text)
+    role = db.Column(db.String(20))  # "jobseeker" or "recruiter"
     location = db.Column(db.String(100))
-    industry = db.Column(db.String(50))
+    profile_data = db.Column(db.JSON)
+
+    def __repr__(self):
+        return f'<User {self.name}>'
+
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recruiter_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    jobseeker_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    status = db.Column(db.String(20))  # "liked", "disliked", "matched"
+    recruiter = db.relationship('User', foreign_keys=[recruiter_id], backref='recruiter_matches')
+    jobseeker = db.relationship('User', foreign_keys=[jobseeker_id], backref='jobseeker_matches')
+
+    def __repr__(self):
+        return f'<Match recruiter_id={self.recruiter_id} jobseeker_id={self.jobseeker_id} status={self.status}>'
